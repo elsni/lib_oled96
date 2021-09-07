cb86e193c05a13096ae6b9259f3e77a32e7c748b
Stephan Elsner <elsni@web.de>
2021-09-07 17:20:24 +0100

    show full weather data

--- showWeatherData.py
+++ showWeatherData.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 # coding=utf-8
-# Bibliotheken importieren
 
+# ---------------------------------------------------------------------------------
+# Lade Aktuelle Wetterdaten vom openweathermap und zeige sie
+# auf einem joyit 0.96" OLED Display an, welches über 12c angesteuert wird.
+# https://joy-it.net/en/products/SBC-OLED01
+
+# Bibliotheken importieren
 import requests
 import json
 from lib_oled96 import ssd1306
@@ -11,11 +16,12 @@ from PIL import ImageFont
 
 
 # die Openwethermap APP-ID
-APPID='b702c266e5dfbee0472987f9c8651d11'
+APPID='<your APP ID>'
 
 # Display einrichten
-i2cbus = SMBus(0)            # 0 = Raspberry Pi 1, Odroid C4, 1 = Raspberry Pi > 1
+i2cbus = SMBus(1)            # 0 = Raspberry Pi 1,Odroid C4, 1 = Raspberry Pi 2,3,4,zero
 oled = ssd1306(i2cbus)
+
 # Ein paar Abkürzungen, um den Code zu entschlacken
 draw = oled.canvas
 
@@ -25,14 +31,17 @@ j = json.loads(r.text)
 outtemp = j['main']['temp']
 outpr= j['main']['pressure']
 outhum=j['main']['humidity']
-				 
 
 # Display zum Start löschen
 oled.cls()
 oled.display()
 
 font = ImageFont.truetype('FreeSans.ttf', 34)
-draw.text((0, 0), f'{outtemp:.1f}°C', font=font, fill=1)    
+draw.text((0, 0), f'{outtemp:.1f}°C', font=font, fill=1)
+font = ImageFont.truetype('FreeSans.ttf', 14)
+draw.text((0, 34), f'{outhum:.1f}%', font=font, fill=1)
+draw.text((0, 48), f'{outpr} hpa', font=font, fill=1)
+
 # Ausgaben auf Display schreiben
 oled.display()
 sleep(10)
